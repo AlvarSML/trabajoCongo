81dedf44170f89345aa3d50d30aed95fe1d2b462
alvarsnow <alvarsnow@gmail.com>
2017-10-24 18:00:18 +0200

    añadidos metodos

---
+++ main.js
@@ -0,0 +1,27 @@
+let pac = [];
+let hosp = [];
+let enfs = [];
+
+function nEmpleado() {
+  //crear empleado
+  //devuelve objeto empleado
+}
+
+function nEnfermedad() {
+  //crear enfermedad
+  //devuelve objeto enfermedad
+}
+
+function nHospital() {
+  //crear hospital
+  //devuelve objeto hospital
+}
+
+function nPaciente() {
+  let nom = prompt("Nombre: ");
+  let edad = prompt("Edad: ");
+  let enfermedad;
+  let personal;
+
+  np = new Paciente(nom,edad,enfermedad,personal);
+}
