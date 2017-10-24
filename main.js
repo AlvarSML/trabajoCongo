f0d66eab77a671d08dd2d7c54e7b6c897df11f75
alvarsnow <alvarsnow@gmail.com>
2017-10-24 17:39:41 +0200

    main añadido

---
+++ main.js
@@ -0,0 +1,16 @@
+let hosp = [];
+
+function nEmpleado() {
+  //crear empleado
+  //devuelve objeto empleado
+}
+
+function nEnfermedad() {
+  //crear enfermedad
+  //devuelve objeto enfermedad
+}
+
+function nHospital() {
+  //crear hospital
+  //devuelve objeto hospital
+}
