3f0615d47f72dd0ba378dfc3302c7c3658553875
alvarsnow <alvarsnow@gmail.com>
2017-10-25 20:54:10 +0200

    funciones pacientes

--- main.js
+++ main.js
@@ -4,28 +4,48 @@ let enfs = [];
 
 function nEmpleado() {
   //crear empleado
-  //devuelve objeto empleado
+  //añade objeto empleado
 }
 
 function nEnfermedad() {
   //crear enfermedad
-  //devuelve objeto enfermedad
+  //añade objeto enfermedad
 }
 
 function nHospital() {
   //crear hospital
-  //devuelve objeto hospital
+  //añade objeto hospital
 }
 
 function nPaciente() {
+  //obtencion de datos
   let nom = prompt("Nombre: ");
   let edad = prompt("Edad: ");
   let enfermedad;
   let personal;
 
+  //creacion del objeto
   np = new Paciente(nom,edad,enfermedad,personal);
+  pac.push(np);
+
+  //actualizacion de la interfaz
+  actualizarPac();
 }
 
-function nPacientes(){
-  document.getElementById('np').value = pac.length()
+function actualizarPac(){
+  document.getElementById('np').value = pac.length;
+}
+
+function verPacientes(){
+  //WI^P
+  let wpac = window.open("pacientes.html");
+  let cont = "<p>";
+
+  for (let i = 0; i < pac.length; i++) {
+    cont += ("<p>" + pac[i].nombre + "</p>");
+
+  }
+
+  cont += "</p>";
+  wpac.document.getElementById('pacs').innerHTML = cont;
 }
