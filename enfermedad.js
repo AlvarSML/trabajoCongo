93c0b01144ff119956b52b4a953f405bf51dc59d
SmockIsNotDead <abluecarrotjt@gmail.com>
2017-10-27 12:46:12 +0200

    constructor y metodos accesores

---
+++ enfermedad.js
@@ -0,0 +1,47 @@
+class enfermedad{
+
+	//constructor de enfermedad, entran el nombre de la enfermedad y los sintomas
+	constructor(nombre, sintomas){
+
+		this.nombre=nombre;
+		this.sintomas=sintomas;
+
+	}
+
+	//Getters____________________________
+
+	//nombre
+	getNombre(){
+		return this.nombre;
+	}
+
+	//sintomas
+	getSintomas(){
+		return this.sintomas;
+	}
+
+	//Setters____________________________
+
+	//nombre
+	setNombre(nombre){
+		this.nombre=nombre;
+	}
+
+
+	//sintomas
+
+	setSintomas(sintomas){
+		this.sintomas=sintomas;
+	}
+
+
+	//insercion
+
+	//modificacion
+
+	//borrado
+
+	//visualizacion
+
+	
+}
